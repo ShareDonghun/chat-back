de97b083394915c8870cd4d288028c26ae48837e
DonghunAn <iandonghun@gmail.com>
2023-01-15 22:30:25 +0900

    Set up express server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 const app: Application = express();
-const PORT = 3000;
+const PORT = 3030;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
